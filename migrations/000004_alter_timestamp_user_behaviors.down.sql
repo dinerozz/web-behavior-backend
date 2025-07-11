3eed19cdfacf28f2cfda5718f61b0a6c5de07214
dinerozz <aznab166lic@gmail.com>
2025-07-11 17:08:05 +0500

    fix: timezone

---
+++ migrations/000004_alter_timestamp_user_behaviors.down.sql
@@ -0,0 +1,11 @@
+ALTER TABLE user_behaviors
+ALTER COLUMN updated_at TYPE TIMESTAMP;
+
+ALTER TABLE user_behaviors
+ALTER COLUMN created_at TYPE TIMESTAMP;
+
+ALTER TABLE user_behaviors
+    ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE user_behaviors
+    ALTER COLUMN updated_at SET DEFAULT CURRENT_TIMESTAMP;
